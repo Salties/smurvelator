04c6b1e89d7a2a408bfaf773b05d1680d8141aa2
saltiny <yanyansmajesty@outlook.com>
2023-10-16 15:44:43 +0000

    Smurf integration.

--- counter/sim_main.cpp
+++ counter/sim_main.cpp
@@ -17,20 +17,38 @@ const int SIM_MAX_TICK = 20;
 
 int main(int argc, char **argv)
 {
+    //Simulation init.
     int count;
     VerilatedContext *contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
-    //Vstimulus *top = new Vstimulus { contextp };
     Vcounter *top = new Vcounter { contextp };
 
+    //Smurf init.
+    Smurf *smurf = InitSmurf("counter.json",
+                             "/tmp/counter.smurf.trace",
+                             SMURF_TRACE_MODE_CREATE);
+    SmurfBind(smurf, "clk", &top->clk);
+    SmurfBind(smurf, "rst", &top->rst);
+    SmurfBind(smurf, "out", &top->out); //TODO: Glitched.
+
+    //Simulate up to SIM_MAX_TICK ticks.
     for (count = 0; (!contextp->gotFinish()) && (count < SIM_MAX_TICK); count++)
     {
         //Set input wires.
         top->rst = 1;
         top->clk ^= 1;
 
+        if(4 == count)          //Rest the counter once.
+        {
+            top->rst = 0;
+        }
+
         //Step once.
         top->eval();
+
+        //Sync and write frame.
+        SmurfSync(smurf);
+        SmurfWrite(smurf);
 
         //Print output.
         printf("%d: %d\n", count, top->out);
